fe456b9aacfb8b0aecc9601d0a693ba938285c55
Adisu W. Terefe <adisu_12@outlook.com>
2021-01-19 16:55:54 +0100

    comments on UserInputReader class

--- Uppgift1/Utils/UserInputReader.cs
+++ Uppgift1/Utils/UserInputReader.cs
@@ -6,20 +6,36 @@ using System.Threading.Tasks;
 
 namespace Uppgift1.Utils
 {
+    /// <summary>
+    /// The UserInputReader class is supposed to be used in the future assignments by extending some of its functions.
+    /// It can be used anywhere in the project as it has generic methods with some validations.
+    /// </summary>
     class UserInputReader
     {
+        /*
+         * Reads a string and returns to the caller
+         */
         public static string ReadString()
         {
             Console.Write("Please Enter name: ");
             return Console.ReadLine();
         }
-
+        /*
+         * Reads a string value from console and returns adouble
+         * if the string is parseable, then it returns the parsed double
+         * otherwise it returns a default value 0.0
+         *
+         */
         public static double ReadDouble()
         {
             Console.Write("Please enter salary: ");
             string number = Console.ReadLine();
             return IsParseable(number) ? double.Parse(number) : 0.0;
         }
+        /*
+         * if the double value is greater than zero then
+         * consider it as valued
+         */
         public static bool IsValidDouble(double value)
         {
             return value >= 0;
@@ -31,17 +47,17 @@ namespace Uppgift1.Utils
             return (double.TryParse(number, out res));
         }
 
+        /*
+         * reads a string and parses it
+         * if successful then return the input
+         * otherwise return default value 0.
+         */
         public static int ReadInt()
         {
-            int i = 0;
+            var i = 0;
             Console.Write("Please Enter id: ");
-            string number = Console.ReadLine();
-            if (int.TryParse(number, out i))
-            {
-                return i;
-            }
-
-            return i;
+            var number = Console.ReadLine();
+            return int.TryParse(number, out i) ? i : 0;
         }
     }
 }
